c7ff4668e13fa53e8b6dcabd2fc3b76127ea5f6c
Karel Hanák <karel.hanak@iqrf.org>
2021-03-21 12:08:36 +0100

    Added syslog logging component
    Signed-off-by: Karel Hanák <karel.hanak@iqrf.org>

---
+++ TraceSyslogService/TraceSyslogService.h
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2021 Logimic,s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "ITraceService.h"
+#include "ITraceFormatService.h"
+#include "ShapeProperties.h"
+#include <string>
+
+namespace shape {
+
+	class TraceSyslogService : public ITraceService {
+  	public:
+		TraceSyslogService();
+		virtual ~TraceSyslogService();
+
+		bool isValid(int level, int chan) const override;
+		void writeMsg(int level, int channel, const char* moduleName, const char* sourceFile, int sourceLine, const char* funcName, const std::string & msg) override;
+		
+		void activate(const Properties *props = 0);
+		void deactivate();
+		void modify(const Properties *props);
+	private:
+		class Imp;
+		Imp * imp;
+	};
+}
