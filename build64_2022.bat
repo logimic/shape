b81c1af1458e20e1c940d916bfaea7bf26708c80
Frantisek Mikulu <frantisek.mikulu@logimic.com>
2022-01-17 14:55:53 +0100

    support MSVS 2022

---
+++ build64_2022.bat
@@ -0,0 +1,25 @@
+set project=shape
+
+rem //expected build dir structure
+set buildexp=build\\VS15_2022_x64
+
+set currentdir=%cd%
+set builddir=.\\%buildexp%
+set deploydir=.\\deploy\\VS15_2022_x64
+
+mkdir %builddir%
+
+rem //get absolute path to to deploydir
+mkdir %deploydir%
+pushd %deploydir%
+set deploydir=%cd%
+popd
+
+rem //launch cmake to generate build environment
+pushd %builddir%
+cmake -G "Visual Studio 17 2022" -DBUILD_TESTING:BOOL=true -DSHAPE_DEPLOY:PATH=%deploydir% %currentdir% 
+popd
+
+rem //build from generated build environment
+cmake --build %builddir% --config Debug --target install
+cmake --build %builddir% --config Release --target install
